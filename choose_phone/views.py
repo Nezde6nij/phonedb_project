5dcdafbbafda9595783168c3f2b582578ce58f69
n0ki$$ <Nezde6nij@gmail.com>
2018-02-04 18:41:17 +1000

    filter page added

--- choose_phone/views.py
+++ choose_phone/views.py
@@ -5,3 +5,8 @@ def index(request):
     template = 'choose_phone/choose_phone.html'
     context = {'hello': 'hello'}
     return render(request, template, context)
+
+def filters(request):
+    template = 'choose_phone/filters.html'
+    context = {'hello': 'hello'}
+    return render(request, template, context)
